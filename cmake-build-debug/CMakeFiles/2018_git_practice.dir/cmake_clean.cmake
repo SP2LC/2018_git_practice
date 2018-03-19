5b75b375414049509ab69cdcff0fa181158bbfad
Natti970 <snao178@gmail.com>
2018-03-19 21:27:33 +0900

    init commit

---
+++ cmake-build-debug/CMakeFiles/2018_git_practice.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2018_git_practice.dir/main.cpp.o"
+  "2018_git_practice.pdb"
+  "2018_git_practice"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2018_git_practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
